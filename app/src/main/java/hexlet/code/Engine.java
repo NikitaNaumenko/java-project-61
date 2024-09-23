b6fef1b01bc0f4cea2426344d7f02b845ef03d1d
Nikita Naumenko <naumenkoniki@gmail.com>
2024-09-23 17:08:48 +0500

    fix scanner

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -68,7 +68,6 @@ final class Engine {
                         userAnswer + " is wrong answer ;(. Correct answer was "
                                 + answer);
                 System.out.println("Let's try again, " + userName);
-                scanner.close();
 
                 return;
             }
